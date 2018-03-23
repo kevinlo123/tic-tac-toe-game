e528f696ffbf85af32422c99872cd9a3900fd187
Kevin <lopezkevin175@gmail.com>
2018-03-22 20:43:06 -0400

    completed the game

--- src/js/tic-tac-toe.js
+++ src/js/tic-tac-toe.js
@@ -3,13 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const resetButton = document.getElementById("reset");
   const chooseOther = "this spot has already been chosen";
   let player = "x";
-  
+  let clickCounter = 0;
+  //board
   let board = [
     0,1,2,
     3,4,5,
     6,7,8,
   ];
-  
+
   const turnLogic = () => {
    return player === "x" ? player = "o" : player = "x";
   }
@@ -17,28 +18,51 @@ document.addEventListener("DOMContentLoaded", () => {
   const winLogic = (currentGame) => {
     switch(true){
       case currentGame[0] === currentGame[1] && currentGame[0] === currentGame[2]:
-        alert("there has been a winner!");
+        clickCounter = 0;
+        alert("There has been a winner!");
         break;
       case currentGame[0] === currentGame[3] && currentGame[0] === currentGame[6]:
-        alert("there has been a winner!");
+        clickCounter = 0;      
+        alert("There has been a winner!");
         break;
       case currentGame[0] === currentGame[4] && currentGame[0] === currentGame[8]:
-        alert("there has been a winner!");
+        clickCounter = 0;      
+        alert("There has been a winner!");
         break;
       case currentGame[1] === currentGame[4] && currentGame[1] === currentGame[7]:
-        alert("there has been a winner!");
+        clickCounter = 0;      
+        alert("There has been a winner!");
         break;
       case currentGame[2] === currentGame[5] && currentGame[2] === currentGame[8]:
-        alert("there has been a winner!");
+        clickCounter = 0;
+        alert("There has been a winner!");
         break;
       case currentGame[3] === currentGame[4] && currentGame[3] === currentGame[5]:
-        alert("there has been a winner!");
+        clickCounter = 0;      
+        alert("There has been a winner!");
         break;
       case currentGame[6] === currentGame[7] && currentGame[6] === currentGame[8]:
-        alert("there has been a winner!");
+        clickCounter = 0;      
+        alert("There has been a winner!");
         break;
       case currentGame[2] === currentGame[4] && currentGame[2] === currentGame[6]:
-        alert("there has been a winner!");
+        clickCounter = 0;
+        alert("There has been a winner!");
+        break;
+    }
+  }
+
+  function tieGameLogic(currentGame){
+    switch(true){
+      case currentGame[0] !== currentGame[1] && currentGame[0] !== currentGame[2] &&
+        currentGame[0] !== currentGame[3] && currentGame[0] !== currentGame[6] &&
+        currentGame[0] !== currentGame[4] && currentGame[0] !== currentGame[8] &&
+        currentGame[1] !== currentGame[4] && currentGame[1] !== currentGame[7] &&
+        currentGame[2] !== currentGame[5] && currentGame[2] !== currentGame[8] &&
+        currentGame[3] !== currentGame[4] && currentGame[3] !== currentGame[5] &&
+        currentGame[6] !== currentGame[7] && currentGame[6] !== currentGame[8] &&
+        currentGame[2] !== currentGame[4] && currentGame[2] !== currentGame[6]:
+        alert("Tie game! No winner!"); 
         break;
     }
   }
@@ -49,19 +73,23 @@ document.addEventListener("DOMContentLoaded", () => {
         player = "x";
         cells[i].innerHTML = player;
         board[i] = player;
-        console.log(board);
+        console.log(board);        
+        clickCounter += 1;              
       }else if(cells[i].innerHTML !== "x"){
         player = "o";
         cells[i].innerHTML = player;
         board[i] = player;
         console.log(board);
+        clickCounter += 1;              
       }
       winLogic(board);
       turnLogic();
+      clickCounter >= 9 ? alert("Tie game! no winner."): console.log("Continue playing");      
     });
-  }
+  } 
 
   resetButton.addEventListener("click" , () => {
+    clickCounter = 0;
     board = [
       0,1,2,
       3,4,5,
