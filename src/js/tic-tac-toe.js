7484f45f080f526e79a10fb2fa9adf222d15cf7c
Kevin <lopezkevin175@gmail.com>
2018-03-27 20:23:51 -0400

    fixed some JS bugs also made made style changes

--- src/js/tic-tac-toe.js
+++ src/js/tic-tac-toe.js
@@ -20,75 +20,47 @@ document.addEventListener("DOMContentLoaded", () => {
       case currentGame[0] === currentGame[1] && currentGame[0] === currentGame[2]:
         clickCounter = 0;
         alert("There has been a winner!");
+        resetBoard();
         break;
       case currentGame[0] === currentGame[3] && currentGame[0] === currentGame[6]:
         clickCounter = 0;      
         alert("There has been a winner!");
+        resetBoard();        
         break;
       case currentGame[0] === currentGame[4] && currentGame[0] === currentGame[8]:
         clickCounter = 0;      
         alert("There has been a winner!");
+        resetBoard();        
         break;
       case currentGame[1] === currentGame[4] && currentGame[1] === currentGame[7]:
         clickCounter = 0;      
         alert("There has been a winner!");
+        resetBoard();        
         break;
       case currentGame[2] === currentGame[5] && currentGame[2] === currentGame[8]:
         clickCounter = 0;
         alert("There has been a winner!");
+        resetBoard();        
         break;
       case currentGame[3] === currentGame[4] && currentGame[3] === currentGame[5]:
         clickCounter = 0;      
         alert("There has been a winner!");
+        resetBoard();        
         break;
       case currentGame[6] === currentGame[7] && currentGame[6] === currentGame[8]:
         clickCounter = 0;      
         alert("There has been a winner!");
+        resetBoard();        
         break;
       case currentGame[2] === currentGame[4] && currentGame[2] === currentGame[6]:
         clickCounter = 0;
         alert("There has been a winner!");
+        resetBoard();        
         break;
     }
   }
 
-  function tieGameLogic(currentGame){
-    switch(true){
-      case currentGame[0] !== currentGame[1] && currentGame[0] !== currentGame[2] &&
-        currentGame[0] !== currentGame[3] && currentGame[0] !== currentGame[6] &&
-        currentGame[0] !== currentGame[4] && currentGame[0] !== currentGame[8] &&
-        currentGame[1] !== currentGame[4] && currentGame[1] !== currentGame[7] &&
-        currentGame[2] !== currentGame[5] && currentGame[2] !== currentGame[8] &&
-        currentGame[3] !== currentGame[4] && currentGame[3] !== currentGame[5] &&
-        currentGame[6] !== currentGame[7] && currentGame[6] !== currentGame[8] &&
-        currentGame[2] !== currentGame[4] && currentGame[2] !== currentGame[6]:
-        alert("Tie game! No winner!"); 
-        break;
-    }
-  }
-
-  for(let i = 0; i < cells.length; i++){
-    cells[i].addEventListener("click", () => {
-      if(cells[i].innerHTML === "" && player === "x"){
-        player = "x";
-        cells[i].innerHTML = player;
-        board[i] = player;
-        console.log(board);        
-        clickCounter += 1;              
-      }else if(cells[i].innerHTML !== "x"){
-        player = "o";
-        cells[i].innerHTML = player;
-        board[i] = player;
-        console.log(board);
-        clickCounter += 1;              
-      }
-      winLogic(board);
-      turnLogic();
-      clickCounter >= 9 ? alert("Tie game! no winner."): console.log("Continue playing");      
-    });
-  } 
-
-  resetButton.addEventListener("click" , () => {
+  const resetBoard = () => {
     clickCounter = 0;
     board = [
       0,1,2,
@@ -99,5 +71,35 @@ document.addEventListener("DOMContentLoaded", () => {
     for(let i = 0; i < cells.length; i++){
       cells[i].innerHTML = "";
     }
-  }); 
+  }
+
+  for(let i = 0; i < cells.length; i++){
+    cells[i].addEventListener("click", () => {
+      switch(true){
+        case cells[i].innerHTML === "" && player === "x":
+          player = "x";
+          cells[i].innerHTML = player;
+          cells[i].style.color = "#EEDA76";
+          board[i] = player;
+          console.log(board);        
+          clickCounter += 1;
+          break;
+        case cells[i].innerHTML !== "x": 
+          player = "o";
+          cells[i].innerHTML = player;
+          cells[i].style.color = "#39ABD9";        
+          board[i] = player;
+          console.log(board);
+          clickCounter += 1;
+          break;
+      }
+      winLogic(board);
+      turnLogic();
+      if(clickCounter >= 9){
+        alert("Tie Game! No winner.");
+        resetBoard();
+      }       
+    });
+  } 
+  resetButton.addEventListener("click" , resetBoard); 
 });
